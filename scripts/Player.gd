feea7d384e3f4e2fa4ce176f693e4935ea4301ea
Adam Damiano <adam@adamlearns.live>
2024-03-08 14:57:50 -0800

    Add some extra movement stuff
    - Pressing "home" will reset your position.
    - Pressing up or down will triple gravity for you.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -22,9 +22,16 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	if Input.is_action_just_pressed("ui_home"):
+		position = Vector2(randi() % 1000, 0)
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction: float = Input.get_axis("ui_left", "ui_right")
+
+	if Input.get_axis("ui_up", "ui_down"):
+		velocity.y += 2 * gravity * delta
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
